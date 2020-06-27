07fb8815b7b581b00d416b8cf0cc1a0ad05e7f66
Josue <josue.ggh@gmail.com>
2020-06-27 16:59:00 -0500

    Other Answer

--- Lesson_3/tapeEquilibrium.js
+++ Lesson_3/tapeEquilibrium.js
@@ -1,22 +1,16 @@
-function solution(A) {
-  const N = A.length;
-  const totalSum = [];
-  totalSum.push(A[0]);
-  for(let i = 1; i< A.length; i++) {
-    totalSum.push(totalSum[i-1] + A[i])
-  }
-  // console.log(A);
-  // console.log(totalSum)
-  let min = Number.MAX_SAFE_INTEGER;
-  const total = totalSum[totalSum.length - 1];
-  for(let p = 0; p < N -1; p++) {
-    const sumLeft = totalSum[p];
-    const sumRight = total - sumLeft;
-    const difference = Math.abs(sumRight - sumLeft);
-    min = Math.min(min, difference);
-  }
-  return min;
-}
+function solution(A){
+	const reducer = (accumulator, currentValue) => accumulator + currentValue; 
+	let MIN = Infinity;
 
+	for(let i= 1; i <  A.length; i++){
+		let left = A.slice(0, i).reduce(reducer);
+		let right = A.slice(i).reduce(reducer);
+		let result = Math.abs(left - right);
+		if(result < MIN  ){
+			MIN = result;
+		}
+	}
+	return MIN;
+}
 const A = [3,1,2,4,3];
 console.log(solution(A));
